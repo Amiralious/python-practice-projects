abb1602a07e1ae909ffa30ff182aa6853903889f
amiralious <amiraliyousefi100@gmail.com>
2026-05-11 02:42:28 +0330

    added practices: f-strings, Type Casting, Casted Value, String Methods

---
+++ fundamentals/basic-user-profile.py
@@ -0,0 +1,33 @@
+# %%
+name = "Ali"
+print (name)
+
+# %%
+age = 25
+print (age)
+
+# %%
+city = "Paris"
+print (city)
+
+# %%
+# f-strings
+print (f"My name is {name}. I am {age} years old and I live in {city}.")
+
+# %%
+# Type Casting
+age_text = "25"
+age_number = int(age_text)
+
+# %%
+# Casted Value
+next_year_age = age_number + 1
+print (f"Next year, {name} will be {next_year_age} years old.")
+
+# %%
+# String Methods
+print (name.upper())
+print (name.lower())
+print (name.title())
+print (name.replace("Ali", "Amir"))
+print(len(name))
