56b2383e48c0d33d8faddbe12c92dc798a478f6b
amiralious <amiraliyousefi100@gmail.com>
2026-05-12 00:40:48 +0330

    Added lists, tuples, set and input to basic-user-profile.py

--- fundamentals/basic-user-profile.py
+++ fundamentals/basic-user-profile.py
@@ -1,12 +1,11 @@
 # %%
+# Print
 name = "Ali"
 print (name)
 
-# %%
 age = 25
 print (age)
 
-# %%
 city = "Paris"
 print (city)
 
@@ -31,3 +30,34 @@ print (name.lower())
 print (name.title())
 print (name.replace("Ali", "Amir"))
 print(len(name))
+
+# %%
+# Lists
+skills = ["python", "git", "linux"]
+skills.append("docker")
+print(skills)
+print(skills[0])
+
+# %%
+# Tuples
+birth_date = (1999, 5, 12)
+print(birth_date)
+print(birth_date[0])
+
+# %%
+# Sets
+raw_skills  = ["python", "git", "python", "linux", "git"]
+unique_skills = set(raw_skills)
+print (unique_skills)
+
+# %%
+# User Input
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+user_age_number = int(user_age)
+
+print (f"Hello {user_name}, next year you will be {user_age_number + 1} years old.")
+
+input ("Where do you live? ")
+print ("Cool!")
+# %%
